96db64c1f35dc8e119dec099e0f74c6172bd652b
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-05-24 19:53:13 -0500

    Add cursor pointer feature, remove console.log

--- src/components/imageSlider/index.jsx
+++ src/components/imageSlider/index.jsx
@@ -36,8 +36,6 @@ export default function ImageSlider({ url, limit = 5, page = 1 }) {
         if (url !== '') fetchImages(url);
     }, [url])
 
-    console.log(images);
-
     if (loading) {
         return <div>Loading...</div>
     }
